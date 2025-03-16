6e4ef6c94820a3375b1a7a2ad5080e002035eddc
testgit <fvamoussajunior@gmailcom>
2025-03-16 03:55:22 +0100

    requests.get() -> requests_get(): qui est une fonction qui nous permet de se connecter en levant des erreurs de connexion pour mettr en pratique try-except-finally

--- src/bdd/db.py
+++ src/bdd/db.py
@@ -4,7 +4,13 @@ import duckdb
 def stocker_dans_bdd(sql, fichier, bdd, nom_table):
     print("Chargement dans la BDD")
     connection = duckdb.connect(bdd)
-    connection.sql(sql)
-    connection.sql(f'INSERT INTO {nom_table} '
-                   f'SELECT * FROM read_json_auto("{fichier}")')
-    connection.close()
+    try:
+        connection.sql(sql)
+        connection.sql(f'INSERT INTO {nom_table} '
+                       f'SELECT * FROM read_json_auto("{fichier}")')
+    except Exception as e: #Doit se placer en dernier, il faut except toutes les heures qui peuvent arriver
+        print(e)
+        raise
+
+    finally:
+        connection.close()
