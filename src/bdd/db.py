17065c111813bb2ad05f5bae56569ae5d8e5303a
testgit <fvamoussajunior@gmailcom>
2025-03-16 13:14:20 +0100

    comprendre l'importance de if __name__ == __main__

--- src/bdd/db.py
+++ src/bdd/db.py
@@ -7,3 +7,6 @@ def stocker_dans_bdd(sql, fichier, bdd, nom_table):
         connection.sql(sql)
         connection.sql(f'INSERT INTO {nom_table} '
                        f'SELECT * FROM read_json_auto("{fichier}")')
+
+if __name__ == '__main__':
+    print("Ceci est un test de stocker_dans_bdd")
