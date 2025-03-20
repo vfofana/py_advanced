211e27316b42284626bf084591ba61c9345df2fd
testgit <fvamoussajunior@gmailcom>
2025-03-20 15:49:46 +0100

    conftest avec les fixtures dyna

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 import pytest
 import logging
-from src.configuration import EconomieGouvConfiguration
+from src.configuration import EconomieGouvConfiguration, DataGouvConfiguration
 
+@pytest.fixture
+def logger_fixture():
+    return logging.getLogger("fixture")
 
 @pytest.fixture
 def fichier_non_existant():
@@ -20,5 +23,21 @@ def economie_gouv_fixture():
     )]
 
 @pytest.fixture
-def logger_fixture():
-    return logging.getLogger("fixture")
+def data_gouv_fixture():
+    return [
+        DataGouvConfiguration(
+            type_api="data_gouv",
+            dataset="dataset",
+            fichier_cible="fichier_cible",
+            fichier_sql="fixtures/sql/test_sql",
+            nom_table="nom_table",
+            sql_creation="SELECT",
+        )
+    ]
+@pytest.fixture
+def empty_list_fixture():
+    return []
+
+@pytest.fixture
+def select_fixture():
+    return 'SELECT'
